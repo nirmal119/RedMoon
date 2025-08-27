62fed5b45d44c5ca730749f4e59a26b26f706e4b
NIRMAL KUMAR <nirmalkumar8273@gmail.com>
2025-08-27 23:25:17 +0530

    Initial commit: RedMoon initialized

---
+++ redmoon/src/main/java/com/example/redmoon/RedMoonApplication.java
@@ -0,0 +1,13 @@
+package com.example.redmoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedMoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedMoonApplication.class, args);
+	}
+
+}
---
+++ redmoon/src/test/java/com/example/redmoon/RedmoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.redmoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedmoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
